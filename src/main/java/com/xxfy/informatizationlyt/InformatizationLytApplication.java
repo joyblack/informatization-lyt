1f14a5704d7928df9087be24fb7a9a2fa27f70fd
joyblack <1016037677@qq.com>
2019-08-09 17:17:00 +0800

    Initial commit

---
+++ src/main/java/com/xxfy/informatizationlyt/InformatizationLytApplication.java
@@ -0,0 +1,13 @@
+package com.xxfy.informatizationlyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformatizationLytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformatizationLytApplication.class, args);
+    }
+
+}
